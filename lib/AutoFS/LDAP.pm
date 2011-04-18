5382df3167ae8432489552cdf92eec0291aeb5d5
Shaun Ashby <Shaun.Ashby@gmail.com>
2011-04-18 23:38:20 +0200

    Add new().

--- lib/AutoFS/LDAP.pm
+++ lib/AutoFS/LDAP.pm
@@ -5,4 +5,12 @@ use strict;
 
 our $VERSION = 0.1;
 
+sub new(){
+	my ($proto) = shift;
+	my ($args) = shift || {}; 
+	my $class = ref($proto) || $proto;
+	return bless({}, $class);
+}
+
 1;
+__END__
