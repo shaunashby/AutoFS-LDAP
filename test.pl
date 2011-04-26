2a3c9b2685c43dcb828d786552754726c4197804
Shaun Ashby <Shaun.Ashby@gmail.com>
2011-04-27 00:00:42 +0200

    Update test script to use Template.

--- test.pl
+++ test.pl
@@ -2,12 +2,23 @@
 
 use strict;
 use warnings;
+use Template;
 
 use AutoFS::Master;
 use AutoFS::Config qw(:all);
 
-my $master = AutoFS::Master->new( { master => AUTOMOUNT_CONFIG_DIR.'/auto_master' });
+my ($template, $output);
+my $master = AutoFS::Master->new( { master => AUTOMOUNT_CONFIG_DIR . '/auto_master' } );
 
-foreach my $table (@{ $master->tables() }) {
-	print $table;
-}
+my $table = $master->getTable('/cons');
+
+$template = Template->new( { } ) || die Template->error(), "\n";
+$template->process(\*DATA, { table => $table }, \$output);
+
+print $output,"\n";
+
+__DATA__
+[% FOR entry IN table.entries -%]
+automountKey=[% entry.mountpoint %]
+automountInformation=[% entry.info %]
+[% END -%]
