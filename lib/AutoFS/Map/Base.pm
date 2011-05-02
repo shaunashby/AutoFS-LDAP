c66329b33635e44267fc4cd9e4b62be5c48c16bc
Shaun Ashby <Shaun.Ashby@unige.ch>
2011-05-02 17:36:08 +0200

    Add base class for automount maps.

---
+++ lib/AutoFS/Map/Base.pm
@@ -0,0 +1,61 @@
+#____________________________________________________________________ 
+# File: Base.pm
+#____________________________________________________________________ 
+#  
+# Author: Shaun ASHBY <Shaun.Ashby@gmail.com>
+# Update: 2011-05-02 17:18:22+0200
+# Revision: $Id$ 
+#
+# Copyright: 2011 (C) Shaun ASHBY
+#
+#--------------------------------------------------------------------
+package AutoFS::Map::Base;
+
+=head1 NAME
+
+AutoFS::Map::Base - Base class for automount maps.
+
+=head1 SYNOPSIS
+
+	my $obj = AutoFS::Map::Base->new();
+
+=head1 DESCRIPTION
+
+This is the base class for automount maps.
+
+=head1 METHODS
+
+=over
+
+=cut
+
+use strict;
+use warnings;
+use Carp;
+
+sub new() {
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = (@_ == 0) ?           # Error if no params given
+	croak("No params given.\n")
+	: (ref($_[0]) eq 'HASH') ? $_[0] # Got hashref already - OK
+	: { @_ };                        # Otherwise, store the params in a hash
+
+    $self->{content} = [];
+    bless($self => $class);
+    return $self;
+}
+
+
+1;
+
+__END__
+
+=back
+
+=head1 AUTHOR/MAINTAINER
+
+Shaun ASHBY
+
+=cut
+
