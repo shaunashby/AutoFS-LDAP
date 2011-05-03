5b8c6404991e9146b70998d4962e59fc5524264d
Shaun Ashby <Shaun.Ashby@unige.ch>
2011-05-03 17:02:36 +0200

    Test dumps correct LDIF file.

--- test.pl
+++ test.pl
@@ -6,19 +6,43 @@ use Template;
 
 use AutoFS::Map::Master;
 
-my ($template, $output);
+use constant AUTOFS_LDAP_BASE_DN => 'dc=integral,dc=ops';
+
+my $outFile = "./autofs.ldif";
+my $template;
 my $master = AutoFS::Map::Master->new( { map_name => "auto_master" } );
 
-my $map = $master->getMap('/cons');
-
 $template = Template->new( { } ) || die Template->error(), "\n";
-$template->process(\*DATA, { map => $map }, \$output);
-
-print $output,"\n";
+$template->process(\*DATA, { master_map => $master, ldap_base_dn => AUTOFS_LDAP_BASE_DN }, $outFile);
 
 __DATA__
-[% FOR entry IN map.entries -%]
-automountKey=[% entry.mountpoint %]
-automountOpts=[% entry.options %]
-automountInformation=[% entry.location %]
+dn: ou=autofs,ou=Services,[% ldap_base_dn %]
+objectClass: top
+objectClass: organizationalUnit
+ou: autofs
+
+dn: automountMapName=auto_master,ou=autofs,ou=Services,[% ldap_base_dn %]
+objectClass: top
+objectClass: automountMap
+automountMapName: auto_master
+
+[% FOR map IN master_map.maps -%]
+
+dn: automountMapName=[% map.map_name %],ou=autofs,ou=Services,[% ldap_base_dn %]
+objectClass: top
+objectClass: automountMap
+automountMapName: [% map.map_name %]
+
+dn: automountKey=[% map.mountpoint %],automountMapName=auto_master,ou=autofs,ou=Services,[% ldap_base_dn %]
+objectClass: automount
+automountInformation: [% map.map_name %]
+automountKey: [% map.mountpoint %]
+
+[% FOR automount IN map.entries %]
+# Mountpoint [% automount.mountpoint %]:
+dn: automountKey=[% automount.key %],automountMapName=[% map.map_name %],ou=autofs,ou=Services,[% ldap_base_dn %]
+objectClass: automount
+automountInformation: [% automount.options %] [% automount.location %]
+automountKey: [% automount.key %]
+[% END %]
 [% END -%]
